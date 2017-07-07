e5346811149939a6647c845eaf1218fb14447d8e
Jeroen Koopman <jkoopman@asp4all.nl>
2017-07-07 09:31:37 +0200

    New version

--- build/quickndirty.ps1
+++ build/quickndirty.ps1
@@ -9,5 +9,6 @@ git commit -am 'New version'
 $remote = git remote
 Write-Verbose -Message "$env:COMPUTERNAME : git remote : $remote" -Verbose
 
-Write-Verbose -Message "$env:COMPUTERNAME : git push $remote master" -Verbose
-git push $remote master
+$remote.ForEach({git push $_ master;
+    Write-Verbose -Message "$env:COMPUTERNAME : git push $_ master" -Verbose
+}) 
